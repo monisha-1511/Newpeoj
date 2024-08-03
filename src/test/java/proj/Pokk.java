8a88cba5d5ae33415ba40148e973fda844b6c7f9
monisha-1511 <monishav415@gmail.com>
2024-08-03 22:00:32 +0530

    7,8,9 added

--- src/test/java/proj/Pokk.java
+++ src/test/java/proj/Pokk.java
@@ -1,6 +1,18 @@
 package proj;
 
 public class Pokk {
+	
+	public void java7() {
+	System.out.println("");
+	}
+
+	public void java8() {
+	System.out.println("");
+	}
+
+	public void java9() {
+	System.out.println("");
+	}
 	public static void main(String[] args) {
 		System.out.println("");
 	}
